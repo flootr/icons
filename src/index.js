24070018450a5adea62957d6ca911ccb4fe92532
Markus Dobmann <markus@mpnjs.com>
2018-09-14 09:32:23 +0200

    don't allow arbitrary icon strings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import qs from "query-string";
 import getIcon from "./icons";
 
-const { icon = "arrow-left" } = qs.parse(location.search);
+let { icon = "arrow-left" } = qs.parse(location.search);
+
+if (!icon.startsWith("arrow")) {
+  icon = "arrow-left";
+}
 
 getIcon(icon).then(icon => {
   document.getElementById("root").innerHTML = icon;
